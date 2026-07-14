718921113b83c3b37f734e8a3db7ce9d989eb93a
fabiof-ff <fabiofersini@gmail.com>
2026-07-14 18:57:31 +0200

    feat: integrate Firebase Analytics and expose premium click tracking via JavascriptInterface

--- ANDROID_APP/android/app/src/main/java/com/ff/ainutritracker/MainActivity.java
+++ ANDROID_APP/android/app/src/main/java/com/ff/ainutritracker/MainActivity.java
@@ -1,5 +1,30 @@
 package com.ff.ainutritracker;
 
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Inizializza Firebase Analytics
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        // Collega l'interfaccia JavaScript AndroidAnalytics alla WebView di Capacitor
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().addJavascriptInterface(new Object() {
+                @JavascriptInterface
+                public void logPremiumClick(String featureName) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("feature_name", featureName);
+                    mFirebaseAnalytics.logEvent("click_premium_interest", bundle);
+                }
+            }, "AndroidAnalytics");
+        }
+    }
+}
